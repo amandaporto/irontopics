e3e66feb359b34d6911e1aaef5f74195871bfe92
Amanda Porto <amandaporto@msn.com>
2015-10-11 23:06:27 -0400

    Added html for topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,24 @@
 class TopicsController < ApplicationController
+  # before_action :authenticate_user!
+  def index
+      @topics = Topic.all
+    end
+
+  def new
+  end
+
+  def create
+    @topic = Topic.create(params[:topic])
+
+    redirect_to @topic
+  end
+
+  def show
+      @topic = Topic.find(params[:id])
+  end
+
+  private
+    def topic_params
+      params.require(:topic).permit(:title, :description, :focus_area)
+    end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,6 @@
 class Topic < ActiveRecord::Base
+  validates :title, presence: true
+  validates :focus_area, presence: true
+  validates :description, presence: true
+
 end

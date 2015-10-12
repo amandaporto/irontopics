4fa33bf7c68d3cda7710b6f4df03b068f1585e91
Amanda Porto <amandaporto@msn.com>
2015-10-11 21:32:26 -0400

    Added topics controller and topic model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012005815) do
+ActiveRecord::Schema.define(version: 20151012012206) do
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "focus_area"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
